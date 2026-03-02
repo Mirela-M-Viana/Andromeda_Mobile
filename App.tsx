16da8777542bf9a2b906d38cf1ff26f8d066cbd9
Mirela <vianamirela.melo@gmail.com>
2026-03-02 18:41:55 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.texto}>Hello World! no APP </Text>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#a9d1ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texto:{
+    fontSize: 30,
+    fontFamily: 'cursive'
+  }
 });

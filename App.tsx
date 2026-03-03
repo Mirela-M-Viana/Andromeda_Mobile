b9fa3ba595deb0aceacf4cb173bf7492674ec0bb
Mirela <vianamirela.melo@gmail.com>
2026-03-02 22:14:43 -0300

    Teste de imagem e texto

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.texto}>Hello World! no APP </Text>
+      {/* <Text style={styles.texto}>Hello World! no APP </Text> */}
       <StatusBar style="light" />
+
+      <Image source={require('./assets/logo.png')} style={styles.logo}/>
+      <Text style={styles.apresentacao} >
+        Andrômeda é uma Joalheria para quem ama o céu, 
+        as estrelas e os misterios do universo!
+        {'\n'} Venha conhecer nossos produtos, e se apaixone pelos 
+        astros cada vez mais!
+      </Text>
     </View>
   );
 }
@@ -13,12 +21,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#a9d1ff',
+    backgroundColor:  'rgb(71, 33, 114)',
     alignItems: 'center',
     justifyContent: 'center',
   },
   texto:{
     fontSize: 30,
     fontFamily: 'cursive'
+  },
+
+  logo:{
+    width: 300,
+    height: 300,
+  },
+
+  apresentacao:{
+    fontSize: 15,
+    width: 300,
+    textAlign: 'center',
+    color: '#ffffff'
+
   }
 });
